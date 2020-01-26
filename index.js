6e00b6d436cdc16b19a87ac2e06b909716385fa8
BibouDV <55670207+BibouDV@users.noreply.github.com>
2020-01-26 11:32:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Client = require('./src/client.js')
 const client = new Client({ fetchAllMembers: true, disableEveryone: true })
 
-client.login(process.env.TOKEN).catch(e => client.log('error', e))
+client.login("NjY3MTQ0NTcyNTEzNTUwMzM2.Xi1mXw.KYKn2MFwNiLvs8CDqhDQxi1U5IE").catch(e => client.log('error', e))
